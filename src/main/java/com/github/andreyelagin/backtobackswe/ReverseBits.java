cefa1aa0671a72d6a211ef712d188eaf0fb8a3d9
Andrey Elagin <daddyingrave@gmail.com>
2021-04-23 00:52:12 +0300

    reverse bits

---
+++ src/main/java/com/github/andreyelagin/backtobackswe/ReverseBits.java
@@ -0,0 +1,16 @@
+package com.github.andreyelagin.backtobackswe;
+
+public class ReverseBits {
+  public static int reverseBits(int input) {
+    int out = 0;
+    while (input != 0) {
+      out = out << 1;
+      System.out.println(Integer.toBinaryString(out));
+      if ((input & 1) == 1)
+        out = out | 1;
+      input = input >> 1;
+    }
+    
+    return out;
+  }
+}
