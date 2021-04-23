dc038fac5a645a8b8048ac12a45706ada2b433c3
Andrey Elagin <daddyingrave@gmail.com>
2021-04-24 00:07:40 +0300

    power of two

---
+++ src/main/java/com/github/andreyelagin/backtobackswe/PowerOfTwo.java
@@ -0,0 +1,19 @@
+package com.github.andreyelagin.backtobackswe;
+
+public class PowerOfTwo {
+  public static boolean powerOfTwo(int input) {
+    return input > 0 && ((input & (input - 1)) == 0);
+  }
+
+  public static boolean myStupidSolution(int input) {
+    if (input == 1) return true;
+    if (input < 2) return false;
+
+    while (input > 1) {
+      if ((input & 1) == 1) return false;
+      input = input >> 1;
+    }
+
+    return true;
+  }
+}
