559478497db330f1d0ccada160c653b57f28e7b9
Andrey Elagin <daddyingrave@gmail.com>
2021-04-25 20:55:17 +0300

    add strings

---
+++ src/test/java/com/github/andreyelagin/backtobackswe/AddStringsTest.java
@@ -0,0 +1,15 @@
+package com.github.andreyelagin.backtobackswe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddStringsTest {
+
+  @Test
+  void addStrings() {
+    assertEquals("102", AddStrings.addStrings("101", "1"));
+    assertEquals("333", AddStrings.addStrings("111", "222"));
+    assertEquals("1100", AddStrings.addStrings("500", "600"));
+  }
+}
