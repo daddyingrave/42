b02df7bfb9a89cb4f78c6f444c34b519a4b2016d
Andrey Elagin <daddyingrave@gmail.com>
2021-04-26 23:57:43 +0300

    minimum time difference

---
+++ src/test/java/com/github/andreyelagin/backtobackswe/MinimumTimeDifferenceTest.java
@@ -0,0 +1,16 @@
+package com.github.andreyelagin.backtobackswe;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MinimumTimeDifferenceTest {
+
+  @Test
+  void timeDifference() {
+    assertEquals(0, MinimumTimeDifference.timeDifference(List.of("00:00", "23:59", "00:00")));
+    assertEquals(4, MinimumTimeDifference.timeDifference(List.of("23:59", "00:03", "12:03")));
+  }
+}
