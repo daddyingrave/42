f109193ff8275ee77becb78e7742e55bda9c7462
Andrey Elagin <daddyingrave@gmail.com>
2021-05-05 00:48:26 +0300

    init matrix traversal

---
+++ src/main/java/com/github/andreyelagin/backtobackswe/arrays/SpiralMatrixTraversal.java
@@ -0,0 +1,15 @@
+package com.github.andreyelagin.backtobackswe.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrixTraversal {
+  public List<Integer> spiralOrder(int[][] matrix) {
+    List<Integer> flatSpiral = new ArrayList<>();
+    int last = matrix.length * matrix[0].length;
+
+    
+    
+    return null;
+  }
+}
