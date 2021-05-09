d9027eaf9d691b92e5a1b233c4b8a37e71f7b721
Andrey Elagin <daddyingrave@gmail.com>
2021-05-09 21:34:48 +0300

    longest palindrome set approach

--- src/main/java/com/github/andreyelagin/backtobackswe/strings/LongestPalindromeConstruction.java
+++ src/main/java/com/github/andreyelagin/backtobackswe/strings/LongestPalindromeConstruction.java
@@ -4,12 +4,32 @@ import java.util.*;
 
 public class LongestPalindromeConstruction {
   public int longestPalindrome(String s) {
+    int counter = 0;
+    Set<Character> chars = new HashSet<>();
+    for (int i = 0; i < s.length(); i++) {
+      char curChar = s.charAt(i);
+      if (!chars.add(curChar)) {
+        chars.remove(curChar);
+        counter++;
+      }
+    }
+
+    counter *= 2;
+    
+    if (!chars.isEmpty()) {
+      counter++;
+    }
+
+    return counter;
+  }
+
+  public int longestPalindromeHash(String s) {
     HashMap<Character, Integer> counts = new HashMap<>();
     for (int i = 0; i < s.length(); i++) {
       char curChar = s.charAt(i);
       counts.compute(curChar, (k, v) -> v == null ? 1 : ++v);
     }
-    
+
     PriorityQueue<Integer> numbers = new PriorityQueue<>(Comparator.reverseOrder());
     boolean flag = false;
     int result = 0;
@@ -28,7 +48,7 @@ public class LongestPalindromeConstruction {
         result += num;
       }
     }
-      
+
     return result;
   }
 }
