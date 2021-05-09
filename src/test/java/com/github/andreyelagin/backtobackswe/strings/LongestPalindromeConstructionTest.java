db313715b0d3cbc2b825ae8da7bd0e9dcbebfbb1
Andrey Elagin <daddyingrave@gmail.com>
2021-05-09 15:29:16 +0300

    longest palindrome naive approach

---
+++ src/test/java/com/github/andreyelagin/backtobackswe/strings/LongestPalindromeConstructionTest.java
@@ -0,0 +1,25 @@
+package com.github.andreyelagin.backtobackswe.strings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestPalindromeConstructionTest {
+
+  LongestPalindromeConstruction longestPalindrome = new LongestPalindromeConstruction();
+
+  @Test
+  void longestPalindrome() {
+    assertEquals(5, longestPalindrome.longestPalindrome("aabbc"));
+    assertEquals(5, longestPalindrome.longestPalindrome("abbcccd"));
+    assertEquals(1, longestPalindrome.longestPalindrome("aA"));
+    assertEquals(1, longestPalindrome.longestPalindrome("xyz"));
+    assertEquals(3, longestPalindrome.longestPalindrome("ccc"));
+    assertEquals(
+        51,
+        longestPalindrome.longestPalindrome(
+            "aaaaabbbbbyyyyyuuuuukkkkklllllfffffrrrrrwwwwwpppppqqqqqabcdefgh"
+        )
+    );
+  }
+}
