9aae19069c9b4f0cc35be33e759318b91d8fe6fa
Andrey Elagin <daddyingrave@gmail.com>
2021-05-10 23:51:09 +0300

    replace words with prefix (improved)

--- src/main/java/com/github/andreyelagin/backtobackswe/strings/ReplaceWordsWithPrefix.java
+++ src/main/java/com/github/andreyelagin/backtobackswe/strings/ReplaceWordsWithPrefix.java
@@ -2,17 +2,36 @@ package com.github.andreyelagin.backtobackswe.strings;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashSet;
 
 public class ReplaceWordsWithPrefix {
   public String replaceWordsWithPrefix(String[] prefixes, String sentence) {
+    var prefs = new HashSet<>(Arrays.asList(prefixes));
+    var words = sentence.split(" ");
+
+    for (int i = 0; i < words.length; i++) {
+      String word = words[i];
+      for (int j = 0; j < word.length(); j++) {
+        var subWord = word.substring(0, j);
+        if (prefs.contains(subWord)) {
+          words[i] = subWord;
+          break;
+        }
+      }
+    }
+
+    return String.join(" ", words);
+  }
+
+  public String replaceWordsWithPrefixMy(String[] prefixes, String sentence) {
     var comparator = Comparator
         .comparing((String str) -> str)
         .thenComparing(String::length);
     Arrays.sort(prefixes, comparator);
-    
+
     var words = sentence.split(" ");
     var res = new StringBuilder();
-    
+
     for (String word : words) {
       boolean matched = false;
       for (String prefix : prefixes) {
