76a0c17db918456680536d7fc82f616e4a70fdc7
Andrey Elagin <daddyingrave@gmail.com>
2021-05-13 23:34:46 +0300

    longest substring without repeating characters (improved)

--- src/main/java/com/github/andreyelagin/backtobackswe/strings/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/github/andreyelagin/backtobackswe/strings/LongestSubstringWithoutRepeatingCharacters.java
@@ -5,17 +5,37 @@ import java.util.HashSet;
 // https://backtobackswe.com/platform/content/longest-substring-without-repeating-characters
 public class LongestSubstringWithoutRepeatingCharacters {
   public int longestUniqueCharacterSubstring(String s) {
+    int left = 0;
+    int right = 0;
+    int max = 0;
+    int[] matches = new int[128];
+
+    while (right < s.length()) {
+      left = Math.max(left, matches[s.charAt(right)]);
+      matches[s.charAt(right)] = right + 1;
+      max = Math.max(right - left + 1, max);
+      right++;
+    }
+
+    return max;
+  }
+  // A B C A B A D E C
+  // left 0, right 0, max 0, []
+  // left 0, right 1, max 1, [A -> 1]
+  // left 0, right 2, max 2, [A -> 1, B - 2]
+
+  public int longestUniqueCharacterSubstringMy(String s) {
     if (s.isEmpty()) {
       return 0;
     }
-    
+
     int left = 0;
     int right = 1;
     int max = 1;
 
     var set = new HashSet<Character>();
     set.add(s.charAt(0));
-    
+
     while (left < s.length() && right < s.length()) {
       if (left == right) {
         right++;
@@ -28,16 +48,7 @@ public class LongestSubstringWithoutRepeatingCharacters {
         set.remove(s.charAt(left++));
       }
     }
-    
+
     return max;
   }
-  // A B C A B A D E C
-  // left 0, right 2, max 2, [A, B]
-  // left 0, right 3, max 3, [A, B, C]
-  // left 1, right 3, max 3, [B, C]
-  // left 1, right 4, max 3, [B, C, A]
-  // left 2, right 4, max 3, [C, A]
-  // left 3, right 4, max 3, [C]
-  // left 3, right 5, max 3, [C, B]
-  // left 3, right 6, max 3, [C, B, A]
 }
