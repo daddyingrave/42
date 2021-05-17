3ae744d962c25ddd66b06622dba363ac5270f26c
Andrey Elagin <daddyingrave@gmail.com>
2021-05-17 23:40:44 +0300

    Add 2 Integers Represented As Linked Lists (fail)

---
+++ src/main/java/com/github/andreyelagin/backtobackswe/linkedlists/AddIntegersRepresentedAsLinkedLists.java
@@ -0,0 +1,34 @@
+package com.github.andreyelagin.backtobackswe.linkedlists;
+
+public class AddIntegersRepresentedAsLinkedLists {
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    var accumulator = new ListNode(666);
+    var accIterator = accumulator;
+
+    var left = l1;
+    var right = l2;
+    int carry = 0;
+
+    while (left != null || right != null) {
+      int sum = carry;
+      if (left != null) {
+        sum += left.val;
+        left = left.next;
+      }
+      if (right != null) {
+        sum += right.val;
+        right = right.next;
+      }
+      if (sum >= 10) {
+        carry = 1;
+        sum = sum % 10;
+      } else {
+        carry = 0;
+      }
+      accIterator.next = new ListNode(sum);
+      accIterator = accIterator.next;
+    }
+
+    return accumulator.next;
+  }
+}
