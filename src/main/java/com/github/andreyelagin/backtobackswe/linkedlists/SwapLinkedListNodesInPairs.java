cd6750edcebe4b848c01ae837d9d01334b98b850
Andrey Elagin <daddyingrave@gmail.com>
2021-05-18 00:26:39 +0300

    Swap Linked List Nodes In Pairs (init)

---
+++ src/main/java/com/github/andreyelagin/backtobackswe/linkedlists/SwapLinkedListNodesInPairs.java
@@ -0,0 +1,8 @@
+package com.github.andreyelagin.backtobackswe.linkedlists;
+
+public class SwapLinkedListNodesInPairs {
+  public ListNode swapInPairs(ListNode head) {
+    
+    return null;
+  }
+}
