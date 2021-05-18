0f500852ff3f2daf6f3cb5a73bd2f3a332c09ed5
Andrey Elagin <daddyingrave@gmail.com>
2021-05-18 23:51:22 +0300

    Swap Linked List Nodes In Pairs (improved)

--- src/main/java/com/github/andreyelagin/backtobackswe/linkedlists/SwapLinkedListNodesInPairs.java
+++ src/main/java/com/github/andreyelagin/backtobackswe/linkedlists/SwapLinkedListNodesInPairs.java
@@ -2,36 +2,29 @@ package com.github.andreyelagin.backtobackswe.linkedlists;
 
 public class SwapLinkedListNodesInPairs {
   public ListNode swapInPairs(ListNode head) {
-    var dummy = new ListNode(666);
-    var left = head;
-    var right = head == null ? null : head.next;
-    
-    if (right == null) {
-      dummy.next = head;
-    } else {
-      dummy.next = right;
+    if (head == null || head.next == null) {
+      return head;
     }
     
-    ListNode prevRight = null;
-    while (left != null && right != null) {
-      var next = right.next;
-      right.next = null;
-      left.next = null;
+    var left = head;
+    var right = head.next;
+    ListNode nextSegment = null;
+    
+    head = head.next;
+    
+    while (true) {
+      nextSegment = right.next;
       right.next = left;
       
-      if (prevRight != null) {
-        prevRight.next = right;
+      if (nextSegment == null || nextSegment.next == null) {
+        left.next = nextSegment;
+        return head;
       }
-      prevRight = left;
       
-      left = next;
-      right = next == null ? null : next.next;
+      left.next = nextSegment.next;
+      
+      left = nextSegment;
+      right = nextSegment.next;
     }
-    
-    if (left != null && prevRight != null) {
-      prevRight.next = left;
-    }
-
-    return dummy.next;
   }
 }
