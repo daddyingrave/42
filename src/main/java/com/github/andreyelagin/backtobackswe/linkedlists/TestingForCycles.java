39e80c8b1162858e8a94497aab5f7e29e0738bf9
Andrey Elagin <daddyingrave@gmail.com>
2021-05-19 23:35:04 +0300

    Testing For Cycles (my solution based on explanation)

--- src/main/java/com/github/andreyelagin/backtobackswe/linkedlists/TestingForCycles.java
+++ src/main/java/com/github/andreyelagin/backtobackswe/linkedlists/TestingForCycles.java
@@ -4,8 +4,28 @@ import java.util.HashSet;
 
 public class TestingForCycles {
   public boolean hasCycle(ListNode head) {
-    var set = new HashSet<Integer>();
+    if (head == null || head.next == null) {
+      return false;
+    }
     
+    var slow = head;
+    var fast = head;
+    while (fast != null) {
+      var curSlow = slow.next;
+      var curFast = fast.next == null ? null : fast.next.next;
+      if (curFast != null && curFast.val == curSlow.val) {
+        return true;
+      }
+      slow = curSlow;
+      fast = curFast;
+    }
+    
+    return false;
+  }
+
+  public boolean hasCycleNaive(ListNode head) {
+    var set = new HashSet<Integer>();
+
     var cur = head;
     while (cur != null) {
       if (!set.add(cur.val)) {
