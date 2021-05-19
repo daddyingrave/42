8fa4298c9f8443c91ba8f5d0188568bb9e3c5f5c
Andrey Elagin <daddyingrave@gmail.com>
2021-05-20 00:38:59 +0300

    Clone A Linked List (init)

---
+++ src/main/java/com/github/andreyelagin/backtobackswe/linkedlists/CloneRandomLinkedList.java
@@ -0,0 +1,8 @@
+package com.github.andreyelagin.backtobackswe.linkedlists;
+
+public class CloneRandomLinkedList {
+  public RandomListNode copyRandomList(RandomListNode head) {
+    
+    return null;
+  }
+}
---
+++ src/main/java/com/github/andreyelagin/backtobackswe/linkedlists/RandomListNode.java
@@ -0,0 +1,10 @@
+package com.github.andreyelagin.backtobackswe.linkedlists;
+
+class RandomListNode {
+  public int val;
+  public RandomListNode next, random;
+
+  public RandomListNode(int val) {
+    this.val = val;
+  }
+}
