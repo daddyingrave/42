283aecdfdafa881106eed2c134d1858016733a4c
Andrey Elagin <daddyingrave@gmail.com>
2021-05-21 00:00:41 +0300

    Clone A Linked List With Random Pointers (links solution)

--- src/main/java/com/github/andreyelagin/backtobackswe/linkedlists/CloneRandomLinkedList.java
+++ src/main/java/com/github/andreyelagin/backtobackswe/linkedlists/CloneRandomLinkedList.java
@@ -4,14 +4,40 @@ import java.util.HashMap;
 
 public class CloneRandomLinkedList {
   public RandomListNode copyRandomList(RandomListNode head) {
+    var cur = head;
+    while (cur != null) {
+      var newNode = new RandomListNode(cur.val);
+      newNode.next = cur.next;
+      cur.next = newNode;
+      cur = newNode.next;
+    }
+
+    cur = head;
+    while (cur != null) {
+      cur.next.random = cur.random != null ? cur.random.next : null;
+      cur = cur.next.next;
+    }
+
+    cur = head;
+    var newHead = head.next;
+    while (cur != null) {
+      var next = cur.next.next;
+      cur.next.next = cur.next.next != null ? cur.next.next.next : null;
+      cur = next;
+    }
+
+    return newHead;
+  }
+
+  public RandomListNode copyRandomListHashMap(RandomListNode head) {
     var copyMap = new HashMap<Integer, RandomListNode>();
-    
+
     var cur = head;
     while (cur != null) {
       copyMap.put(cur.val, new RandomListNode(cur.val));
       cur = cur.next;
     }
-    
+
     cur = head;
     while (cur != null) {
       var newNode = copyMap.get(cur.val);
