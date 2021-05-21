83632a798fc406a9bc11d139a691d2e7e7e0eeec
Andrey Elagin <daddyingrave@gmail.com>
2021-05-22 00:39:46 +0300

    Sublist Reversal (not my :( )

--- src/main/java/com/github/andreyelagin/backtobackswe/linkedlists/SublistReversal.java
+++ src/main/java/com/github/andreyelagin/backtobackswe/linkedlists/SublistReversal.java
@@ -2,13 +2,42 @@ package com.github.andreyelagin.backtobackswe.linkedlists;
 
 public class SublistReversal {
   public ListNode reverseBetween(ListNode head, int start, int end) {
+    if (start == end) {
+      return head;
+    }
+
+    var dummy = new ListNode(666);
+    dummy.next = head;
+    
+    var nodeBeforeSublist = dummy;
+    int index = 1;
+    while (index < start) {
+      nodeBeforeSublist = nodeBeforeSublist.next;
+      index++;
+    }
+    
+    var sublistPointer = nodeBeforeSublist.next;
+    while (start < end) {
+      var front = sublistPointer.next;
+      sublistPointer.next = front.next;
+      
+      front.next = nodeBeforeSublist.next;
+      nodeBeforeSublist.next = front;
+      
+      start++;
+    }
+    
+    return dummy.next;
+  }
+
+  public ListNode reverseBetweenMy(ListNode head, int start, int end) {
     ListNode prevSub = null;
     ListNode nextSub = null;
     int index = 1;
 
     ListNode dummy = new ListNode(666);
     dummy.next = head;
-    
+
     ListNode cur = head;
     ListNode prev = dummy;
     while (cur != null) {
@@ -21,10 +50,10 @@ public class SublistReversal {
 
       prev = cur;
       cur = cur.next;
-      
+
       index++;
     }
-    
+
     index = start;
     cur = prevSub.next;
     prev = null;
@@ -37,17 +66,17 @@ public class SublistReversal {
       if (index == end) {
         subHead = cur;
       }
-      
+
       var next = cur.next;
       cur.next = prev;
       prev = cur;
       cur = next;
-      
+
       index++;
     }
     prevSub.next = subHead;
     subTail.next = nextSub;
-    
+
     return dummy.next;
   }
 }
