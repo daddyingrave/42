e3e076e72ee79422bb836fa9611671a399e3ba0c
Andrey Elagin <daddyingrave@gmail.com>
2021-05-24 11:11:09 +0300

    Implement A Queue With A Max API (init)

---
+++ src/main/java/com/github/andreyelagin/backtobackswe/stacksandqueues/QueueMax.java
@@ -0,0 +1,15 @@
+package com.github.andreyelagin.backtobackswe.stacksandqueues;
+
+public class QueueMax {
+  public void enqueue(int x) {
+    
+  }
+  
+  public int dequeue() {
+    return 666;
+  }
+  
+  public int max() {
+    return 666;
+  }
+}
