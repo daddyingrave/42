9d5a123384d1a055fc0894d12f220366c0450809
Andrey Elagin <daddyingrave@gmail.com>
2021-05-25 23:12:49 +0300

    Minimum Removals for Valid String (my)

--- src/main/java/com/github/andreyelagin/backtobackswe/stacksandqueues/MinimumRemovalsValidString.java
+++ src/main/java/com/github/andreyelagin/backtobackswe/stacksandqueues/MinimumRemovalsValidString.java
@@ -1,12 +1,44 @@
 package com.github.andreyelagin.backtobackswe.stacksandqueues;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Stack;
 
 public class MinimumRemovalsValidString {
   public String makeStringValid(String s) {
-    var stack = new Stack<Character>();
+    var openParenthesis = new Stack<Integer>();
+    var indexesToRemove = new ArrayList<Integer>();
+
+    for (int i = 0; i < s.length(); i++) {
+      var curChar = s.charAt(i);
+      if (curChar == '(') {
+        openParenthesis.push(i);
+      } else if (curChar == ')') {
+        if (!openParenthesis.isEmpty()) {
+          openParenthesis.pop();
+        } else {
+          indexesToRemove.add(i);
+        }
+      }
+    }
+    while (!openParenthesis.isEmpty()) {
+      indexesToRemove.add(openParenthesis.pop());
+    }
+
+    Collections.sort(indexesToRemove);
+    var arr = s.toCharArray();
+
+    for (Integer index : indexesToRemove) {
+      arr[index] = '*';
+    }
+
+    var sb = new StringBuilder();
+    for (char c : arr) {
+      if (c != '*') {
+        sb.append(c);
+      }
+    }
     
-    
-    return null;
+    return sb.toString();
   }
 }
