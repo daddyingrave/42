a4a91df1ecfb93fc0b2237bf68c539bf47dc002b
Andrey Elagin <daddyingrave@gmail.com>
2021-05-27 00:20:00 +0300

    Root To Leaf Path Sum (improved)

--- src/main/java/com/github/andreyelagin/backtobackswe/tree/SumRootToLeafPaths.java
+++ src/main/java/com/github/andreyelagin/backtobackswe/tree/SumRootToLeafPaths.java
@@ -2,6 +2,18 @@ package com.github.andreyelagin.backtobackswe.tree;
 
 public class SumRootToLeafPaths {
   public boolean hasPathSum(TreeNode node, int targetSum) {
+    if (node == null) {
+      return false;
+    }
+    if (targetSum - node.val == 0 && node.left == null && node.right == null) {
+      return true;
+    }
+
+    return hasPathSum(node.left, targetSum - node.val)
+        || hasPathSum(node.right, targetSum - node.val);
+  }
+
+  public boolean hasPathSumMy(TreeNode node, int targetSum) {
     return sumTraverse(node, 0, targetSum);
   }
 
