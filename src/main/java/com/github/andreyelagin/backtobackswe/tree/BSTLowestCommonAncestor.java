0bfdc878c4cc0ba1a48c15a0700df943f1b1fd5b
Andrey Elagin <daddyingrave@gmail.com>
2021-05-29 23:32:07 +0300

    Lowest Common Ancestor In A BST (improved) add test

--- src/main/java/com/github/andreyelagin/backtobackswe/tree/BSTLowestCommonAncestor.java
+++ src/main/java/com/github/andreyelagin/backtobackswe/tree/BSTLowestCommonAncestor.java
@@ -2,16 +2,10 @@ package com.github.andreyelagin.backtobackswe.tree;
 
 public class BSTLowestCommonAncestor {
   public TreeNode lowestCommonAncestor(TreeNode root, int x, int y) {
-    return findCommonAncestor(root, x, y);
-  }
-
-  private TreeNode findCommonAncestor(TreeNode root, int x, int y) {
-    if (root != null && ((root.val < x && root.val > y) || (root.val > x && root.val < y))) {
-      return root;
-    } else if (root != null && root.val > x) {
-      return findCommonAncestor(root.left, x, y);
-    } else if (root != null && root.val < x) {
-      return findCommonAncestor(root.right, x, y);
+    if (root != null && root.val > x && root.val > y) {
+      return lowestCommonAncestor(root.left, x, y);
+    } else if (root != null && root.val < x && root.val < y) {
+      return lowestCommonAncestor(root.right, x, y);
     }
     return root;
   }
