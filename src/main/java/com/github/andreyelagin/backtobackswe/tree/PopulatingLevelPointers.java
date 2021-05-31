67077a9243f6720fba95b9ed49aebca1fbdf9f39
Andrey Elagin <daddyingrave@gmail.com>
2021-06-01 01:12:58 +0300

    Populating Level Pointers (init)

---
+++ src/main/java/com/github/andreyelagin/backtobackswe/tree/PopulatingLevelPointers.java
@@ -0,0 +1,8 @@
+package com.github.andreyelagin.backtobackswe.tree;
+
+public class PopulatingLevelPointers {
+  public TreeNodeNext populate(TreeNodeNext root) {
+    
+    return null;
+  }
+}
---
+++ src/main/java/com/github/andreyelagin/backtobackswe/tree/TreeNodeNext.java
@@ -0,0 +1,8 @@
+package com.github.andreyelagin.backtobackswe.tree;
+
+public class TreeNodeNext extends TreeNode {
+  public TreeNode next;
+  public TreeNodeNext(int val) {
+    super(val);
+  }
+}
