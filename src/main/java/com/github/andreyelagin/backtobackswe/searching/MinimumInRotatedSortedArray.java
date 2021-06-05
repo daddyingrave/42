42cf9ffb5ea65183e8baa87c4be429609bbd5a84
Andrey Elagin <daddyingrave@gmail.com>
2021-06-06 01:07:22 +0300

    Minimum Item In A Rotated Sorted Array

---
+++ src/main/java/com/github/andreyelagin/backtobackswe/searching/MinimumInRotatedSortedArray.java
@@ -0,0 +1,20 @@
+package com.github.andreyelagin.backtobackswe.searching;
+
+public class MinimumInRotatedSortedArray {
+  public int findMin(int[] nums) {
+    int mid = nums.length / 2;
+    int l = 0;
+    int r = nums.length - 1;
+
+    while (l < r) {
+      mid = l + ((r - l) / 2);
+      if (nums[mid] > nums[r]) {
+        l = mid + 1;
+      } else {
+        r = mid;
+      }
+    }
+
+    return nums.length > 0 ? nums[l] : -1;
+  }
+}
