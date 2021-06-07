ba7d0709c3e502da014aaf87225db3c7ee76c7a2
Andrey Elagin <daddyingrave@gmail.com>
2021-06-07 15:22:40 +0300

    K Smallest Elements In An Array

---
+++ src/main/java/com/github/andreyelagin/backtobackswe/heaps/KSmallestElementsInArray.java
@@ -0,0 +1,22 @@
+package com.github.andreyelagin.backtobackswe.heaps;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class KSmallestElementsInArray {
+  public List<Integer> kSmallestElements(int[] elements, int k) {
+    var result = new ArrayList<Integer>();
+    var heap = new PriorityQueue<Integer>();
+
+    for (int element : elements) {
+      heap.offer(element);
+    }
+
+    for (int i = 0; i < k; i++) {
+      result.add(heap.poll());
+    }
+    
+    return result;
+  }
+}
