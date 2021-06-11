38180237c24be29bb6555c28e8e801e489538a34
Andrey Elagin <daddyingrave@gmail.com>
2021-06-11 23:13:50 +0300

    Longest Substring with At Most K Characters

---
+++ src/test/java/com/github/andreyelagin/backtobackswe/hashtables/LongestSubstringMostKCharactersTest.java
@@ -0,0 +1,19 @@
+package com.github.andreyelagin.backtobackswe.hashtables;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestSubstringMostKCharactersTest {
+
+  LongestSubstringMostKCharacters longest = new LongestSubstringMostKCharacters();
+  
+  @Test
+  void longestSubstringWithAtMostKDistinctCharacters() {
+    assertEquals(
+        4,
+        longest.longestSubstringWithAtMostKDistinctCharacters("coffee", 2)
+    );
+    
+  }
+}
