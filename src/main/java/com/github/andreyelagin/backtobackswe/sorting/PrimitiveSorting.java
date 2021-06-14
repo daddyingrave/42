5d4f6cf6f7858369fd63a8dcd6204e6be96a1bcf
Andrey Elagin <daddyingrave@gmail.com>
2021-06-14 18:42:07 +0300

    Naive Sorting Algorithms

--- src/main/java/com/github/andreyelagin/backtobackswe/sorting/PrimitiveSorting.java
+++ src/main/java/com/github/andreyelagin/backtobackswe/sorting/PrimitiveSorting.java
@@ -1,40 +1,54 @@
 package com.github.andreyelagin.backtobackswe.sorting;
 
+import java.util.Arrays;
+
 public class PrimitiveSorting {
-  public static void bubble(int[] arr) {
-    int i = 1;
-    int j = arr.length - 1;
+  public static int[] bubble(int[] arr) {
+    var copy = Arrays.copyOf(arr, arr.length);
     
-    while (j >= 0) {
-      while (i <= j) {
-        if (arr[i] < arr[i - 1]) {
-          int temp = arr[i];
-          arr[i] = arr[i - 1];
-          arr[i - 1] = temp;
+    for (int i = copy.length - 1; i >= 0; i--) {
+      for (int j = 1; j <= i; j++) {
+        if (copy[j] < copy[j - 1]) {
+          int temp = copy[j];
+          copy[j] = copy[j - 1];
+          copy[j - 1] = temp;
         }
-        i++;
       }
-      i = 1;
-      j--;
     }
+    
+    return copy;
   }
 
-  public static void insertion(int[] arr) {
-    int i = 1;
+  public static int[] insertion(int[] arr) {
+    var copy = Arrays.copyOf(arr, arr.length);
     
-    while (i < arr.length) {
-      int cur = arr[i];
-      int j = i - 1;
-      while (j >= 0 && arr[j] > cur) {
-        arr[j + 1] = arr[j];
-        j--;
+    for (int i = 1; i < copy.length; i++) {
+      int cur = copy[i];
+      int j;
+      for (j = i - 1; j >= 0 && copy[j] > cur; j--) {
+        copy[j + 1] = copy[j];
       }
-      arr[j + 1] = cur;
-      i++;
+      copy[j + 1] = cur;
     }
+    
+    return copy;
   }
 
-  public static void selection(int[] arr) {
+  public static int[] selection(int[] arr) {
+    var copy = Arrays.copyOf(arr, arr.length);
+
+    for (int i = copy.length - 1; i > 0; i--) {
+      int largest = 0;
+      for (int j = 1; j <= i; j++) {
+        if (copy[largest] < copy[j]) {
+          largest = j;
+        }
+      }
+      int temp = copy[i];
+      copy[i] = copy[largest];
+      copy[largest] = temp;
+    }
     
+    return copy;
   }
 }
