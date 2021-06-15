e6587e041f8d06f85700cba689f57e7e58ccb372
Andrey Elagin <daddyingrave@gmail.com>
2021-06-16 00:16:14 +0300

    Sort A K Sorted Array

---
+++ src/main/java/com/github/andreyelagin/backtobackswe/sorting/SortKSortedArray.java
@@ -0,0 +1,24 @@
+package com.github.andreyelagin.backtobackswe.sorting;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class SortKSortedArray {
+  public int[] sortNearlySortedArray(int[] arr, int k) {
+    var result = new int[arr.length];
+    var minHeap = new PriorityQueue<Integer>();
+    
+    for (int j = 0; j < k && j < arr.length; j++) {
+      minHeap.offer(arr[j]);
+    }
+
+    for (int i = 0; i < result.length; i++) {
+      if (k < result.length) {
+        minHeap.offer(arr[k++]);
+      }
+      result[i] = minHeap.poll();
+    }
+
+    return result;
+  }
+}
