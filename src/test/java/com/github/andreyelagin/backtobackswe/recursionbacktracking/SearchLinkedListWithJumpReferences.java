be002f2f84b3cfe93936beed7c7109add167c6ce
Andrey Elagin <daddyingrave@gmail.com>
2021-06-19 13:48:10 +0300

    Search A Linked List With Jump References add tests

--- src/test/java/com/github/andreyelagin/backtobackswe/recursionbacktracking/SearchLinkedListWithJumpReferences.java
+++ src/test/java/com/github/andreyelagin/backtobackswe/recursionbacktracking/SearchLinkedListWithJumpReferences.java
@@ -1,29 +1,92 @@
 package com.github.andreyelagin.backtobackswe.recursionbacktracking;
 
+import java.util.Objects;
+
 public class SearchLinkedListWithJumpReferences {
   public void setJumpReferences(ListNodeJump node) {
-    Integer order = 0;
+    Int order = new Int(0);
     traverseAndSet(node, order);
   }
 
-  private void traverseAndSet(ListNodeJump node, Integer order) {
-    if (node == null || node.val == -1) {
+  private void traverseAndSet(ListNodeJump node, Int order) {
+    if (node == null || node.val != -1) {
       return;
     }
-    
-    node.val = order++;
-    
+
+    node.val = order.val;
+    order.val += 1;
+
     traverseAndSet(node.jump, order);
     traverseAndSet(node.next, order);
   }
 
-  private static class ListNodeJump {
+  static class Int {
+    int val;
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Int anInt = (Int) o;
+      return val == anInt.val;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(val);
+    }
+
+    public Int(int val) {
+      this.val = val;
+    }
+  }
+
+  static class ListNodeJump {
     public int val;
     public ListNodeJump next;
     public ListNodeJump jump;
 
     public ListNodeJump(int val) {
       this.val = val;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      ListNodeJump that = (ListNodeJump) o;
+
+      var thisCur = this;
+      var thatCur = that;
+
+      while (thisCur != null && thatCur != null) {
+        if (thisCur.val != thatCur.val) {
+          return false;
+        }
+        thisCur = thisCur.next;
+        thatCur = thatCur.next;
+      }
+
+      return thisCur == null && thatCur == null;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(val);
+    }
+
+    @Override
+    public String toString() {
+      var sb = new StringBuilder();
+      var cur = this;
+
+      int stupidCounter = 0;
+      while (cur != null) {
+        sb.append(cur.val).append(" ");
+        cur = cur.next;
+      }
+
+      return sb.toString();
     }
   }
 }
