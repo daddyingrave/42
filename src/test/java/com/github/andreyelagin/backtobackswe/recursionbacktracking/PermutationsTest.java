ecd80beb0e17d7e1fb65aa71706f513424b3b901
Andrey Elagin <daddyingrave@gmail.com>
2021-06-21 00:50:33 +0300

    Permutations

---
+++ src/test/java/com/github/andreyelagin/backtobackswe/recursionbacktracking/PermutationsTest.java
@@ -0,0 +1,26 @@
+package com.github.andreyelagin.backtobackswe.recursionbacktracking;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PermutationsTest {
+
+  Permutations permutations = new Permutations();
+
+  @Test
+  void permute() {
+    var expected = List.of(
+        List.of(1, 2, 3),
+        List.of(1, 3, 2),
+        List.of(2, 1, 3),
+        List.of(2, 3, 1),
+        List.of(3, 1, 2),
+        List.of(3, 2, 1)
+    );
+
+    assertTrue(expected.containsAll(permutations.permute(new int[]{1, 2, 3})));
+  }
+}
