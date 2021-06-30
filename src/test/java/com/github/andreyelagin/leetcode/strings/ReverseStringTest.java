6144ed83d0988dbf7f32dbca4cdab3424b274d40
Andrey Elagin <daddyingrave@gmail.com>
2021-07-01 00:19:41 +0300

    344. Reverse String

---
+++ src/test/java/com/github/andreyelagin/leetcode/strings/ReverseStringTest.java
@@ -0,0 +1,27 @@
+package com.github.andreyelagin.leetcode.strings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseStringTest {
+
+  ReverseString reverseString = new ReverseString();
+
+  @Test
+  void reverseString() {
+    char[] str = new char[]{'h', 'e', 'l', 'l', 'o'};
+    reverseString.reverseString(str);
+    assertArrayEquals(
+        new char[]{'o', 'l', 'l', 'e', 'h'},
+        str
+    );
+
+    char[] str2 = new char[]{'H', 'a', 'n', 'n', 'a', 'h'};
+    reverseString.reverseString(str2);
+    assertArrayEquals(
+        new char[]{'h','a','n','n','a','H'},
+        str2
+    );
+  }
+}
