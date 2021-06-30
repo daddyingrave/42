4dd5e4597d1512d45fb3bb5b28acf1d3aba4393a
Andrey Elagin <daddyingrave@gmail.com>
2021-06-30 12:23:04 +0300

    66. Plus One

---
+++ src/main/java/com/github/andreyelagin/leetcode/arrays/PlusOne.java
@@ -0,0 +1,18 @@
+package com.github.andreyelagin.leetcode.arrays;
+
+public class PlusOne {
+  public int[] plusOne(int[] digits) {
+    for (int i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] == 9) {
+        digits[i] = 0;
+      } else {
+        digits[i]++;
+        return digits;
+      }
+    }
+    int[] copy = new int[digits.length + 1];
+    copy[0] = 1;
+
+    return copy;
+  }
+}
