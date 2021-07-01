3e95e945363503618f84f07323e8b0a3a5c8e5c1
Andrey Elagin <daddyingrave@gmail.com>
2021-07-02 00:43:42 +0300

    206. Reverse Linked List

---
+++ src/main/java/com/github/andreyelagin/leetcode/linkedlists/ReverseLinkedList.java
@@ -0,0 +1,21 @@
+package com.github.andreyelagin.leetcode.linkedlists;
+
+public class ReverseLinkedList {
+  public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) {
+      return head;
+    }
+
+    ListNode prev = null;
+    ListNode cur = head;
+    
+    while (cur != null) {
+      var next = cur.next;
+      cur.next = prev;
+      prev = cur;
+      cur = next;
+    }
+    
+    return prev;
+  }
+}
