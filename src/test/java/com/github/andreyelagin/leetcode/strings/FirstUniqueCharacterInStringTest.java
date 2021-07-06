6c0f8d449e568aa77e56d5de4f9af2283a92221f
Andrey Elagin <daddyingrave@gmail.com>
2021-07-06 23:58:39 +0300

    387. First Unique Character in a String

---
+++ src/test/java/com/github/andreyelagin/leetcode/strings/FirstUniqueCharacterInStringTest.java
@@ -0,0 +1,17 @@
+package com.github.andreyelagin.leetcode.strings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FirstUniqueCharacterInStringTest {
+
+  FirstUniqueCharacterInString inString = new FirstUniqueCharacterInString();
+  
+  @Test
+  void firstUniqChar() {
+    assertEquals(0, inString.firstUniqChar("leetcode"));
+    assertEquals(2, inString.firstUniqChar("loveleetcode"));
+    assertEquals(-1, inString.firstUniqChar("aabb"));
+  }
+}
