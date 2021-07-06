334d6c27fb5b5c5972693803a952fef06cd79850
Andrey Elagin <daddyingrave@gmail.com>
2021-07-07 00:36:28 +0300

    242. Valid Anagram (improved)

--- src/main/java/com/github/andreyelagin/leetcode/strings/ValidAnagram.java
+++ src/main/java/com/github/andreyelagin/leetcode/strings/ValidAnagram.java
@@ -1,28 +1,20 @@
 package com.github.andreyelagin.leetcode.strings;
 
-import java.util.HashMap;
-
 public class ValidAnagram {
   public boolean isAnagram(String s, String t) {
     if (s.length() != t.length()) {
       return false;
     }
-    
-    var sCount = new HashMap<Character, Integer>();
-    var tCount = new HashMap<Character, Integer>();
-    
-    for (int i = 0; i < s.length(); i++) {
-      sCount.compute(s.charAt(i), (kek, val) -> val == null ? 1 : val + 1);
-      tCount.compute(t.charAt(i), (kek, val) -> val == null ? 1 : val + 1);
-    }
+
+    int[] count = new int[26];
 
     for (int i = 0; i < s.length(); i++) {
-      char ch = s.charAt(i);
-      Integer sNum = sCount.get(ch);
-      Integer tNum = tCount.get(ch);
-      if (sNum == null || tNum == null) {
-        return false;
-      } else if (!sNum.equals(tNum)) {
+      count[s.charAt(i) - 'a']++;
+      count[t.charAt(i) - 'a']--;
+    }
+
+    for (int j : count) {
+      if (j != 0) {
         return false;
       }
     }
