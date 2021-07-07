b812bae6d8b1c4437459b9aee4186b138633076f
Andrey Elagin <daddyingrave@gmail.com>
2021-07-08 00:36:12 +0300

    19. Remove Nth Node From End of List (improved)

--- src/main/java/com/github/andreyelagin/leetcode/linkedlists/RemoveNthNodeFromEnd.java
+++ src/main/java/com/github/andreyelagin/leetcode/linkedlists/RemoveNthNodeFromEnd.java
@@ -2,6 +2,33 @@ package com.github.andreyelagin.leetcode.linkedlists;
 
 public class RemoveNthNodeFromEnd {
   public ListNode removeNthFromEnd(ListNode head, int n) {
+    ListNode dummy = new ListNode();
+    dummy.next = head;
+    
+    ListNode prev = null;
+    ListNode distant = dummy;
+    ListNode cur = dummy;
+    int distance = 0;
+    
+    while (cur.next != null) {
+      var curTemp = cur;
+      cur = cur.next;
+      distance++;
+      if (distance >= n) {
+        prev = distant;
+        distant = distant.next;
+        distance--;
+      }
+    }
+    
+    if (prev != null) {
+      prev.next = distant.next;
+    }
+    
+    return dummy.next;
+  }
+  
+  public ListNode removeNthFromEnd2(ListNode head, int n) {
     int size = 0;
     var cur = head;
     while (cur.next != null) {
