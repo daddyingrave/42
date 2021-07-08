4fa596991ead04a018d9673c77c3a3faa54dc099
Andrey Elagin <daddyingrave@gmail.com>
2021-07-09 00:53:24 +0300

    98. Validate Binary Search Tree

---
+++ src/main/java/com/github/andreyelagin/leetcode/trees/ValidateBinarySearchTree.java
@@ -0,0 +1,19 @@
+package com.github.andreyelagin.leetcode.trees;
+
+public class ValidateBinarySearchTree {
+  public boolean isValidBST(TreeNode root) {
+    return bst(root, null, null);
+  }
+
+  public boolean bst(TreeNode root, Integer max, Integer min) {
+    if (root == null) {
+      return true;
+    }
+    
+    if (max != null && root.val >= max || min != null && root.val <= min) {
+      return false;
+    }
+
+    return bst(root.right, max, root.val) && bst(root.left, root.val, min);
+  }
+}
