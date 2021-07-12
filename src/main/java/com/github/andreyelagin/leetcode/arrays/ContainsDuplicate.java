ec17e798a72bd1391496ea132ffb7da538785c46
Andrey Elagin <daddyingrave@gmail.com>
2021-07-13 00:22:44 +0300

    217. Contains Duplicate

---
+++ src/main/java/com/github/andreyelagin/leetcode/arrays/ContainsDuplicate.java
@@ -0,0 +1,31 @@
+package com.github.andreyelagin.leetcode.arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+  public boolean containsDuplicate(int[] nums) {
+    var set = new HashSet<Integer>();
+    for (int i = 0; i < nums.length; i++) {
+      if (set.contains(nums[i])) {
+        return true;
+      } else {
+        set.add(nums[i]);
+      }
+    }
+    
+    return false;
+  }
+
+  // much faster
+  public boolean containsDuplicateSorted(int[] nums) {
+    Arrays.sort(nums);
+    for (int i = 1; i < nums.length; i++) {
+      if (nums[i] == nums[i - 1]) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
---
+++ src/test/java/com/github/andreyelagin/leetcode/arrays/ContainsDuplicateTest.java
@@ -0,0 +1,17 @@
+package com.github.andreyelagin.leetcode.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainsDuplicateTest {
+
+  ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+
+  @Test
+  void containsDuplicate() {
+    assertTrue(containsDuplicate.containsDuplicate(new int[]{1, 2, 3, 1}));
+    assertTrue(containsDuplicate.containsDuplicate(new int[]{1, 1, 1, 3, 3, 4, 3, 2, 4, 2}));
+    assertFalse(containsDuplicate.containsDuplicate(new int[]{1, 2, 3, 4}));
+  }
+}
