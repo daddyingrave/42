8b2f47ec1b7fbf02f3a5c0a264a11d4acf57f381
Andrey Elagin <daddyingrave@gmail.com>
2021-07-13 00:11:00 +0300

    26. Remove Duplicates from Sorted Array

---
+++ src/test/java/com/github/andreyelagin/leetcode/arrays/RemoveDuplicatesFromSortedArrayTest.java
@@ -0,0 +1,19 @@
+package com.github.andreyelagin.leetcode.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveDuplicatesFromSortedArrayTest {
+
+  RemoveDuplicatesFromSortedArray duplicates = new RemoveDuplicatesFromSortedArray();
+  
+  @Test
+  void removeDuplicates() {
+    var arr1 = new int[]{1, 1, 2};
+    var arr2 = new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+    assertEquals(2, duplicates.removeDuplicates(arr1));
+    assertEquals(5, duplicates.removeDuplicates(arr2));
+  }
+}
