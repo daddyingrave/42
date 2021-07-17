17ac3498429ad56198b2399df0430a04cdd4adce
Andrey Elagin <daddyingrave@gmail.com>
2021-07-18 00:56:12 +0300

    349. Intersection of Two Arrays

---
+++ src/test/java/com/github/andreyelagin/leetcode/arrays/IntersectionOfTwoArraysIITest.java
@@ -0,0 +1,26 @@
+package com.github.andreyelagin.leetcode.arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntersectionOfTwoArraysIITest {
+
+  IntersectionOfTwoArraysII intersection = new IntersectionOfTwoArraysII();
+
+  @Test
+  void intersect() {
+    assertArrayEquals(
+        new int[]{2, 2},
+        intersection.intersect(
+            new int[]{1, 2, 2, 1},
+            new int[]{2, 2}
+        ));
+    assertArrayEquals(
+        new int[]{4, 9},
+        intersection.intersect(
+            new int[]{4, 9, 5},
+            new int[]{9, 4, 9, 8, 4}
+        ));
+  }
+}
