817b6777ca86ad050b9de52fe65cabcf9f541b2b
Andrey Elagin <daddyingrave@gmail.com>
2021-07-18 00:40:43 +0300

    136. Single Number

---
+++ src/main/java/com/github/andreyelagin/leetcode/arrays/SingleNumber.java
@@ -0,0 +1,12 @@
+package com.github.andreyelagin.leetcode.arrays;
+
+public class SingleNumber {
+  public int singleNumber(int[] nums) {
+    int first = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+      first = first ^ nums[i];
+    }
+    
+    return first;
+  }
+}
