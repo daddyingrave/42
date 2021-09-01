5baf67ecc9e9df2e7c1dd5b483f2d67cc73b5198
Andrey Elagin <daddyingrave@gmail.com>
2021-09-01 23:56:53 +0300

    [ctci] String compression (improve a bit)

--- src/main/java/com/github/andreyelagin/ctci/arraysandstrings/StringCompression.java
+++ src/main/java/com/github/andreyelagin/ctci/arraysandstrings/StringCompression.java
@@ -3,6 +3,7 @@ package com.github.andreyelagin.ctci.arraysandstrings;
 import java.util.HashMap;
 
 public class StringCompression {
+  // count compressed length in advance
   public String compress(String source) {
     var lettersCount = new HashMap<Character, Integer>();
     var sb = new StringBuilder();
@@ -18,14 +19,18 @@ public class StringCompression {
       } else {
         prevCount++;
       }
+
+      if (sb.length() >= source.length()) {
+        return source;
+      }
     }
 
     sb.append(prev).append(prevCount);
 
     if (sb.length() >= source.length()) {
       return source;
-    } else {
-      return sb.toString();
     }
+    
+    return sb.toString();
   }
 }
