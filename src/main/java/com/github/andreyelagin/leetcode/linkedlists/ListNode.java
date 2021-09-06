498fd6b49e0585e4f9d7e36e732d01350729a365
Andrey Elagin <daddyingrave@gmail.com>
2021-09-07 00:15:09 +0300

    2. Add Two Numbers

--- src/main/java/com/github/andreyelagin/leetcode/linkedlists/ListNode.java
+++ src/main/java/com/github/andreyelagin/leetcode/linkedlists/ListNode.java
@@ -1,5 +1,8 @@
 package com.github.andreyelagin.leetcode.linkedlists;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListNode {
   int val;
   ListNode next;
@@ -14,5 +17,29 @@ public class ListNode {
   ListNode(int val, ListNode next) {
     this.val = val;
     this.next = next;
+  }
+  
+  public List<Integer> toList() {
+    var list = new ArrayList<Integer>();
+    
+    var cur = this;
+    while (cur != null) {
+      list.add(cur.val);
+      cur = cur.next;
+    }
+    
+    return list;
+  }
+
+  public static ListNode fromList(List<Integer> source) {
+    var dummy = new ListNode();
+    var head = dummy;
+
+    for (Integer v : source) {
+      head.next = new ListNode(v);
+      head = head.next;
+    }
+    
+    return dummy.next;
   }
 }
