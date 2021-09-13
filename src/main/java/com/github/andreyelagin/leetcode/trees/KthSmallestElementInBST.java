0cef649836b8f0f436cfed83110a5fbc5322c68f
Andrey Elagin <daddyingrave@gmail.com>
2021-09-13 17:51:00 +0300

    230. Kth Smallest Element in a BST

---
+++ src/main/java/com/github/andreyelagin/leetcode/trees/KthSmallestElementInBST.java
@@ -0,0 +1,32 @@
+package com.github.andreyelagin.leetcode.trees;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class KthSmallestElementInBST {
+  public int kthSmallest(TreeNode root, int k) {
+    var heap = new PriorityQueue<Integer>(Comparator.reverseOrder());
+
+    traverse(root, k, heap);
+    
+    return heap.peek();
+  }
+
+  private void traverse(TreeNode node, int k, Queue<Integer> heap) {
+    if (node == null) {
+      return;
+    }
+    
+    int cur = node.val;
+    if (heap.size() < k) {
+      heap.offer(cur);
+    } else if (heap.size() > 0 && heap.peek() > cur) {
+      heap.remove();
+      heap.offer(cur);
+    }
+
+    traverse(node.left, k, heap);
+    traverse(node.right, k, heap);
+  }
+}
