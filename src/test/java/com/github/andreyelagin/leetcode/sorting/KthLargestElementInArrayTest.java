46c2dbae71d8608c2f138a6e3ac3548a03065c1c
Andrey Elagin <daddyingrave@gmail.com>
2021-09-16 23:50:46 +0300

    215. Kth Largest Element in an Array (heap)

---
+++ src/test/java/com/github/andreyelagin/leetcode/sorting/KthLargestElementInArrayTest.java
@@ -0,0 +1,16 @@
+package com.github.andreyelagin.leetcode.sorting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KthLargestElementInArrayTest {
+
+  KthLargestElementInArray solution = new KthLargestElementInArray();
+
+  @Test
+  void findKthLargest() {
+    assertEquals(5, solution.findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+    assertEquals(4, solution.findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+  }
+}
