22ed9d3fe0a832aa21ce96212a27ad2894eee6e3
Andrey Elagin <daddyingrave@gmail.com>
2021-09-20 00:34:28 +0300

    56. Merge Intervals

---
+++ src/test/java/com/github/andreyelagin/leetcode/sorting/MergeIntervalsTest.java
@@ -0,0 +1,17 @@
+package com.github.andreyelagin.leetcode.sorting;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MergeIntervalsTest {
+
+  MergeIntervals solution = new MergeIntervals();
+  
+  @Test
+  void merge() {
+    assertEquals(
+        Set
+    );
+  }
+}
