d1f098070e7e9b26cec9e05fdfeed6605ec1afc1
Andrey Elagin <daddyingrave@gmail.com>
2021-09-23 00:44:57 +0300

    297. Serialize and Deserialize Binary Tree

---
+++ src/test/java/com/github/andreyelagin/leetcode/design/SerializeAndDeserializeBinaryTreeTest.java
@@ -0,0 +1,27 @@
+package com.github.andreyelagin.leetcode.design;
+
+import com.github.andreyelagin.leetcode.trees.TreeNode;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SerializeAndDeserializeBinaryTreeTest {
+
+  SerializeAndDeserializeBinaryTree solution = new SerializeAndDeserializeBinaryTree();
+  
+  @Test
+  void serialize() {
+    var n1 = new TreeNode(1);
+    n1.left = new TreeNode(2);
+    n1.left.left = new TreeNode(-3);
+    n1.right = new TreeNode(3);
+    n1.right.left = new TreeNode(4);
+    n1.right.right = new TreeNode(5);
+    
+    System.out.println(solution.serialize(n1));
+  }
+
+  @Test
+  void deserialize() {
+  }
+}
