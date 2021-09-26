8355a2abb2ed88f5426759f3d0e9042eb46f9fa3
Andrey Elagin <daddyingrave@gmail.com>
2021-09-26 23:05:14 +0300

    69. Sqrt(x)

---
+++ src/main/java/com/github/andreyelagin/leetcode/math/Sqrt.java
@@ -0,0 +1,11 @@
+package com.github.andreyelagin.leetcode.math;
+
+public class Sqrt {
+  public int mySqrt(int x) {
+    long r = x;
+    while (r * r > x) {
+      r = (r + x / r) / 2;
+    }
+    return (int) r;
+  }
+}
