52344640c5530d015da02d4f080594026257340f
Andrey Elagin <daddyingrave@gmail.com>
2021-09-28 00:24:48 +0300

    169. Majority Element (updated)

--- src/main/java/com/github/andreyelagin/leetcode/arrays/MajorityElement.java
+++ src/main/java/com/github/andreyelagin/leetcode/arrays/MajorityElement.java
@@ -1,25 +1,21 @@
 package com.github.andreyelagin.leetcode.arrays;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MajorityElement {
   public int majorityElement(int[] nums) {
-    var hashTable = new HashMap<Integer, Integer>();
+    int majority = nums[0];
+    int count = 1;
 
-    for (int i = 0; i < nums.length; i++) {
-      hashTable.compute(nums[i], (k, v) -> v == null ? 1 : ++v);
-    }
-
-    Map.Entry<Integer, Integer> max = null;
-    for (Map.Entry<Integer, Integer> entry : hashTable.entrySet()) {
-      if (max == null) {
-        max = entry;
+    for (int i = 1; i < nums.length; i++) {
+      if (majority == nums[i]) {
+        count++;
+      } else if (count == 0) {
+        majority = nums[i];
+        count++;
       } else {
-        max = entry.getValue() > max.getValue() ? entry : max;
+        count--;
       }
     }
 
-    return max.getKey();
+    return majority;
   }
 }
