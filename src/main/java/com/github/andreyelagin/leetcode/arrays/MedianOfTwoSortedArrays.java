5ed022234dc067ddf87e771c46d4ecc8ecdbee31
Andrey Elagin <daddyingrave@gmail.com>
2021-10-03 23:30:55 +0300

    4. Median of Two Sorted Arrays (solution is waaay more clever than me)

--- src/main/java/com/github/andreyelagin/leetcode/arrays/MedianOfTwoSortedArrays.java
+++ src/main/java/com/github/andreyelagin/leetcode/arrays/MedianOfTwoSortedArrays.java
@@ -1,36 +1,43 @@
 package com.github.andreyelagin.leetcode.arrays;
 
+import static java.lang.Double.NEGATIVE_INFINITY;
+import static java.lang.Double.POSITIVE_INFINITY;
+
 public class MedianOfTwoSortedArrays {
   public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-    int[] merged = new int[nums1.length + nums2.length];
-    
-    int n1 = 0;
-    int n2 = 0;
-    int m = 0;
-    while (n1 < nums1.length && n2 < nums2.length) {
-      if (nums1[n1] <= nums2[n2]) {
-        merged[m] = nums1[n1++];
-      } else {
-        merged[m] = nums2[n2++];
-      }
-      m++;
-    }
-    
-    if (n1 < nums1.length) {
-      while (n1 < nums1.length) {
-        merged[m++] = nums1[n1++];
-      }
+    int[] a, b;
+    if (nums1.length > nums2.length) {
+      a = nums2;
+      b = nums1;
     } else {
-      while (n2 < nums2.length) {
-        merged[m++] = nums2[n2++];
-      }
+      a = nums1;
+      b = nums2;
     }
 
-    int mid = merged.length / 2;
-    if (merged.length % 2 == 0) {
-      return ((double) (merged[mid] + merged[mid - 1])) / 2;
-    } else {
-      return merged[mid];
+    int half = (a.length + b.length) / 2;
+    int left = 0;
+    int right = a.length - 1;
+
+    while (true) {
+      int leftMid = Math.floorDiv(left + right, 2);
+      int rightMid = half - leftMid - 2;
+
+      double aLeft = leftMid >= 0 ? a[leftMid] : NEGATIVE_INFINITY;
+      double aRight = (leftMid + 1) < a.length ? a[leftMid + 1] : POSITIVE_INFINITY;
+      double bLeft = rightMid >= 0 ? b[rightMid] : NEGATIVE_INFINITY;
+      double bRight = (rightMid + 1) < b.length ? b[rightMid + 1] : POSITIVE_INFINITY;
+
+      if (aLeft <= bRight && bLeft <= aRight) {
+        if ((a.length + b.length) % 2 == 0) {
+          return ((Math.max(aLeft, bLeft) + Math.min(aRight, bRight))) / 2;
+        } else {
+          return Math.min(aRight, bRight);
+        }
+      } else if (aLeft > bRight) {
+        right = leftMid - 1;
+      } else {
+        left = leftMid + 1;
+      }
     }
   }
 }
