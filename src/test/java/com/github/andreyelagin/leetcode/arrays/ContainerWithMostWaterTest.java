a215e657d95f897c27e38fce82b0c19430838a66
Andrey Elagin <daddyingrave@gmail.com>
2021-10-05 00:47:11 +0300

    11. Container With Most Water

--- src/test/java/com/github/andreyelagin/leetcode/arrays/ContainerWithMostWaterTest.java
+++ src/test/java/com/github/andreyelagin/leetcode/arrays/ContainerWithMostWaterTest.java
@@ -10,10 +10,12 @@ class ContainerWithMostWaterTest {
 
   @Test
   void maxArea() {
-//    assertEquals(1, solution.maxArea(new int[]{1, 1}));
-//    assertEquals(16, solution.maxArea(new int[]{4, 3, 2, 1, 4}));
-//    assertEquals(2, solution.maxArea(new int[]{1, 2, 1}));
-//    assertEquals(49, solution.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    assertEquals(1, solution.maxArea(new int[]{1, 1}));
+    assertEquals(16, solution.maxArea(new int[]{4, 3, 2, 1, 4}));
+    assertEquals(2, solution.maxArea(new int[]{1, 2, 1}));
+    assertEquals(49, solution.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
     assertEquals(17, solution.maxArea(new int[]{2, 3, 4, 5, 18, 17, 6}));
+    assertEquals(1, solution.maxArea(new int[]{1, 2}));
+    assertEquals(24, solution.maxArea(new int[]{1, 3, 2, 5, 25, 24, 5}));
   }
 }
