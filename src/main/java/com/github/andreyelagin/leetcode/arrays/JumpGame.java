64939e85a0f1aaa1dd393640ab0d78528bdaa73e
Andrey Elagin <daddyingrave@gmail.com>
2021-10-06 00:34:51 +0300

    55. Jump Game

--- src/main/java/com/github/andreyelagin/leetcode/arrays/JumpGame.java
+++ src/main/java/com/github/andreyelagin/leetcode/arrays/JumpGame.java
@@ -2,16 +2,31 @@ package com.github.andreyelagin.leetcode.arrays;
 
 public class JumpGame {
   public boolean canJump(int[] nums) {
-    return traverse(nums, 0);
-  }
-
-  private boolean traverse(int[] nums, int index) {
-    if (index >= nums.length - 1) {
+    if (nums.length == 1 && nums[0] == 0) {
       return true;
-    } else if (nums[index] == 0) {
-      return false;
     }
 
-    return traverse(nums, index + nums[index]--) || traverse(nums, index + nums[index]);
+    int[] dp = new int[nums.length];
+
+    for (int i = 0; i <= nums[0] && i < nums.length; i++) {
+      dp[i] = 1;
+    }
+
+    for (int i = 1; i < nums.length; i++) {
+      if (dp[i] == 0) {
+        return false;
+      }
+      int currentIndex = i + 1;
+      int iterations = nums[i];
+      while (currentIndex < nums.length && iterations > 0) {
+        dp[currentIndex++] = 1;
+        iterations--;
+      }
+      if (dp[dp.length - 1] == 1) {
+        return true;
+      }
+    }
+
+    return dp[dp.length - 1] == 1;
   }
 }
--- src/test/java/com/github/andreyelagin/leetcode/arrays/JumpGameTest.java
+++ src/test/java/com/github/andreyelagin/leetcode/arrays/JumpGameTest.java
@@ -14,7 +14,10 @@ class JumpGameTest {
     assertTrue(solution.canJump(new int[]{2, 0}));
     assertTrue(solution.canJump(new int[]{2, 5, 0, 0}));
     assertTrue(solution.canJump(new int[]{5, 9, 3, 2, 1, 0, 2, 3, 3, 1, 0, 0}));
+    assertTrue(solution.canJump(new int[]{0}));
+    assertTrue(solution.canJump(new int[]{1, 2}));
+
     assertFalse(solution.canJump(new int[]{3, 2, 1, 0, 4}));
-    assertFalse(solution.canJump(new int[]{3, 2, 1, 0, 4}));
+    assertFalse(solution.canJump(new int[]{0, 2, 1}));
   }
 }
