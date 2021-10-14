a35b7f8734f546e1734532ff0fe3b3e4db8d7bb7
Andrey Elagin <daddyingrave@gmail.com>
2021-10-14 23:50:42 +0300

    31. Next Permutation (init)

---
+++ src/main/java/com/github/andreyelagin/leetcode/arrays/NextPermutation.java
@@ -0,0 +1,14 @@
+package com.github.andreyelagin.leetcode.arrays;
+
+public class NextPermutation {
+  public void nextPermutation(int[] nums) {
+    int last = nums.length - 1;
+    int prev = last - 1;
+
+    while (last > 0) {
+      if (nums[prev] < nums[last]) {
+        
+      }
+    }
+  }
+}
