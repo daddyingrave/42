28dc374a9a2653c5306da8d568ede6b719c3d4b9
Andrey Elagin <daddyingrave@gmail.com>
2021-11-02 00:16:14 +0300

    39. Combination Sum

---
+++ src/main/java/com/github/andreyelagin/leetcode/backtracking/CombinationSum.java
@@ -0,0 +1,36 @@
+package com.github.andreyelagin.leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CombinationSum {
+  public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    Arrays.sort(candidates);
+    var result = new ArrayList<List<Integer>>();
+
+    backtrack(target, 0, candidates, new LinkedList<>(), result);
+
+    return result;
+  }
+
+  private void backtrack(
+      int left,
+      int start,
+      int[] nums,
+      LinkedList<Integer> buf,
+      List<List<Integer>> res) {
+    if (left == 0) {
+      res.add(new ArrayList<>(buf));
+    } else if (left < 0) {
+      return;
+    }
+
+    for (int i = start; i < nums.length; i++) {
+      buf.add(nums[i]);
+      backtrack(left - nums[i], i, nums, buf, res);
+      buf.removeLast();
+    }
+  }
+}
