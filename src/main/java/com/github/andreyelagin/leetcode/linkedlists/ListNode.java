eea155730248606a0be92c5781232ff685ca8e4b
Andrey Elagin <daddyingrave@gmail.com>
2022-08-01 23:48:25 +0400

    24. Swap Nodes in Pairs

--- src/main/java/com/github/andreyelagin/leetcode/linkedlists/ListNode.java
+++ src/main/java/com/github/andreyelagin/leetcode/linkedlists/ListNode.java
@@ -2,6 +2,7 @@ package com.github.andreyelagin.leetcode.linkedlists;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ListNode {
   public int val;
@@ -18,16 +19,16 @@ public class ListNode {
     this.val = val;
     this.next = next;
   }
-  
+
   public List<Integer> toList() {
     var list = new ArrayList<Integer>();
-    
+
     var cur = this;
     while (cur != null) {
       list.add(cur.val);
       cur = cur.next;
     }
-    
+
     return list;
   }
 
@@ -39,7 +40,45 @@ public class ListNode {
       head.next = new ListNode(v);
       head = head.next;
     }
-    
+
     return dummy.next;
+  }
+
+  @Override
+  public String toString() {
+    var sb = new StringBuffer();
+
+    sb.append("[").append(val);
+
+    if (next != null) {
+      sb.append(" -> ");
+    }
+
+    var n = next;
+    while (n != null) {
+      sb.append(n.val);
+      if (n.next != null) {
+        sb.append(" -> ");
+      }
+      n = n.next;
+    }
+
+    sb.append("]");
+
+    return sb.toString();
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    ListNode listNode = (ListNode) o;
+
+    return this.toString().equals(listNode.toString());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(val);
   }
 }
---
+++ src/test/java/com/github/andreyelagin/leetcode/linkedlists/SwapNodesInPairsTest.java
@@ -0,0 +1,21 @@
+package com.github.andreyelagin.leetcode.linkedlists;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SwapNodesInPairsTest {
+
+  SwapNodesInPairs swapNodesInPairs = new SwapNodesInPairs();
+
+  @Test
+  void swapPairs() {
+    ListNode head = ListNode.fromList(List.of(1, 2, 3, 4));
+
+    var expected = ListNode.fromList(List.of(2, 1, 4, 3));
+
+    assertEquals(expected, swapNodesInPairs.swapPairs(head));
+  }
+}
