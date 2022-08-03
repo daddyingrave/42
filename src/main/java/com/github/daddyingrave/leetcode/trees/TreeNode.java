f4e15cc55d7a656de3108b08eedcba0dbcfa776a
Andrey Elagin <daddyingrave@gmail.com>
2022-08-03 23:24:32 +0400

    add toString for TreeNode

--- src/main/java/com/github/daddyingrave/leetcode/trees/TreeNode.java
+++ src/main/java/com/github/daddyingrave/leetcode/trees/TreeNode.java
@@ -1,5 +1,9 @@
 package com.github.daddyingrave.leetcode.trees;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
 public class TreeNode {
   public int val;
   public TreeNode left;
@@ -17,11 +21,49 @@ public class TreeNode {
     this.left = left;
     this.right = right;
   }
-  
+
   @Override
   public String toString() {
-    return "TreeNode{" +
-        "val=" + val +
-        '}';
+    var d = new LinkedHashMap<Integer, List<String>>();
+    rec(this, 0, d);
+
+    var sb = new StringBuilder();
+    int indent = d.size() + 1;
+
+    d.forEach((lvl, nodes) -> {
+      sb.append(" ".repeat(indent - lvl));
+      nodes.forEach(n -> sb.append(n).append(" "));
+      sb.append("\n");
+    });
+
+    return sb.toString();
+  }
+
+  private void rec(TreeNode h, int level, LinkedHashMap<Integer, List<String>> storage) {
+    if (h == null) {
+      storage.compute(level, (lvl, row) -> {
+        if (row == null) {
+          var r = new ArrayList<String>();
+          r.add("n");
+          return r;
+        } else {
+          row.add("n");
+          return row;
+        }
+      });
+    } else {
+      storage.compute(level, (lvl, row) -> {
+        if (row == null) {
+          var r = new ArrayList<String>();
+          r.add(Integer.toString(h.val));
+          return r;
+        } else {
+          row.add(Integer.toString(h.val));
+          return row;
+        }
+      });
+      rec(h.left, level + 1, storage);
+      rec(h.right, level + 1, storage);
+    }
   }
 }
