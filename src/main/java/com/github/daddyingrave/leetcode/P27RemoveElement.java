8529f741dbc97f836e870472c2d554d1e677c079
Andrey Elagin <daddyingrave@gmail.com>
2024-09-10 23:44:14 +0200

    27 optimal

--- src/main/java/com/github/daddyingrave/leetcode/P27RemoveElement.java
+++ src/main/java/com/github/daddyingrave/leetcode/P27RemoveElement.java
@@ -1,6 +1,18 @@
 package com.github.daddyingrave.leetcode;
 
 public class P27RemoveElement {
+  // Optimal
+  // public int removeElement(int[] nums, int val) {
+  //        int index = 0;
+  //        for (int i = 0; i < nums.length; i++) {
+  //            if (nums[i] != val) {
+  //                nums[index] = nums[i];
+  //                index++;
+  //            }
+  //        }
+  //        return index;
+  //    }
+
   public int removeElement(int[] nums, int val) {
     int currentIndex = 0;
     int latestIndex = nums.length - 1;
