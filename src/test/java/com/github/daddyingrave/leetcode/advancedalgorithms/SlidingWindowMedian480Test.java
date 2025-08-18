aea6a2c21afae52323afe9e9a1d83cc30dae2177
Andrey Elagin <daddyingrave@gmail.com>
2025-08-18 22:34:32 +0200

    here we go again

--- src/test/java/com/github/daddyingrave/leetcode/advancedalgorithms/SlidingWindowMedian480Test.java
+++ src/test/java/com/github/daddyingrave/leetcode/advancedalgorithms/SlidingWindowMedian480Test.java
@@ -2,66 +2,12 @@ package com.github.daddyingrave.leetcode.advancedalgorithms;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.PriorityQueue;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SlidingWindowMedian480Test {
   class Solution {
     public double[] medianSlidingWindow(int[] nums, int k) {
-      var minHeap = new PriorityQueue<Integer>();
-      var maxHeap = new PriorityQueue<Integer>(Comparator.reverseOrder());
-      var toDelete = new HashMap<Integer, Integer>();
-      double[] result = new double[nums.length - k + 1];
-
-      for (int i = 0; i < k; i++) {
-        maxHeap.add(nums[i]);
-      }
-      for (int i = 0; i < k / 2; i++) {
-        minHeap.add(maxHeap.poll());
-      }
-
-      int resultIndex = 0;
-      if (k % 2 == 0) {
-        result[resultIndex++] = (maxHeap.peek() + minHeap.peek()) / 2.0;
-      } else {
-        result[resultIndex++] = (double) maxHeap.peek();
-      }
-
-      for (int i = k; i < nums.length; i++) {
-        toDelete.merge(nums[i - k], 1, Integer::sum);
-
-        if (!minHeap.isEmpty() && nums[i] > minHeap.peek()) {
-          minHeap.add(nums[i]);
-        } else {
-          maxHeap.add(nums[i]);
-        }
-
-        if (minHeap.size() - maxHeap.size() > 1) {
-          maxHeap.add(minHeap.poll());
-        } else if (maxHeap.size() - minHeap.size() > 1) {
-          minHeap.add(maxHeap.poll());
-        }
-
-        while (!minHeap.isEmpty() && toDelete.getOrDefault(minHeap.peek(), 0) > 0) {
-          toDelete.put(minHeap.peek(), toDelete.get(minHeap.peek()) - 1);
-          minHeap.poll();
-        }
-        while (!maxHeap.isEmpty() && toDelete.getOrDefault(maxHeap.peek(), 0) > 0) {
-          toDelete.put(maxHeap.peek(), toDelete.get(maxHeap.peek()) - 1);
-          maxHeap.poll();
-        }
-
-        if (k % 2 == 0) {
-          result[resultIndex++] = ((double) maxHeap.peek() + minHeap.peek()) / 2.0;
-        } else {
-          result[resultIndex++] = (double) maxHeap.peek();
-        }
-      }
-
-      return result;
+      return null;
     }
   }
 
