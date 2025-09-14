477d07e7c3e332ff8a97fe84dcb03c64575f2dfc
Andrey Elagin <daddyingrave@gmail.com>
2025-09-14 16:51:56 +0200

    here we go again

---
+++ src/test/java/com/github/daddyingrave/leetcode/advancedalgorithms/LongestPalindromicSubsequence516Test.java
@@ -0,0 +1,35 @@
+package com.github.daddyingrave.leetcode.advancedalgorithms;
+
+import org.junit.jupiter.api.Test;
+
+public class LongestPalindromicSubsequence516Test {
+  class Solution {
+    public int longestPalindromeSubseq(String s) {
+      int n = s.length();
+      int[] dp = new int[n];
+
+      for (int i = n - 1; i >= 0; i--) {
+        dp[i] = 1;
+        int prev = 0;
+        for (int j = i + 1; j < n; j++) {
+          int temp = dp[j];
+
+          if (s.charAt(i) == s.charAt(j)) {
+            dp[j] = prev + 2;
+          } else {
+            dp[j] = Math.max(dp[j], dp[j - 1]);
+          }
+
+          prev = temp;
+        }
+      }
+
+      return dp[n - 1];
+    }
+  }
+
+  @Test
+  void test() {
+
+  }
+}
